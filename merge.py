2964b9a056240629cc4e7e14170e72dedc9b0714
Luis Pedro Coelho <luis@luispedro.org>
2012-02-28 18:29:39 +0000

    ENH Add --ignore-flags option
    This, as the name says, ignores the flags on the files and looks at
    content only.
    
    Also, look at flags before content if using both (much faster)

--- merge.py
+++ merge.py
@@ -15,7 +15,7 @@ def hash_file(filename):
         s = input.read(4096)
     content = hash.hexdigest()
     return content
-    
+
 def props_for(filename):
     '''
     props = props_for(filename)
@@ -26,12 +26,21 @@ def props_for(filename):
     return st.st_mode, st.st_uid, st.st_gid, st.st_mtime
 
 
-def merge(origin, dest):
+def merge(origin, dest, ignore_flags=False):
     '''
     for op,args in merge(origin, dest);
         op(*args)
 
     Attempt to merge directories `origin` and `dest`
+
+    Parameters
+    ----------
+    origin : str
+        path to origin
+    dest : str
+        path to destination
+    ignore_flags : bool, optional
+        whether to ignore flags (default: False)
     '''
     filequeue = os.listdir(origin)
     while filequeue:
@@ -42,13 +51,13 @@ def merge(origin, dest):
             yield os.rename, (ofname, dfname)
             continue
         if path.isdir(ofname):
-            filequeue.extend(path.join(ofname,ch) for ch in os.listdir(ofname))
+            filequeue.extend(path.join(fname,ch) for ch in os.listdir(ofname))
+            continue
+        if not ignore_flags and props_for(ofname) != props_for(dfname):
+            print 'Flags differ: %s' % (fname)
             continue
         if hash_file(ofname) != hash_file(dfname):
             print 'Content differs: %s' % (fname)
-            continue
-        if props_for(ofname) != props_for(dfname):
-            print 'Flags differ: %s' % (fname) 
             continue
         yield os.unlink, (ofname,)
 
@@ -62,7 +71,8 @@ Merges directory <origin> into directory <dest>
         sys.exit(0)
     origin = argv[1]
     dest = argv[2]
-    for op,args in merge(origin, dest):
+    ignore_flags = (len(argv) > 3 and argv[3] == '--ignore-flags')
+    for op,args in merge(origin, dest, ignore_flags=ignore_flags):
         op(*args)
 
 
