3bdefca8a83140dd68bf4374bf009e9c73de3e42
Luis Pedro Coelho <luis@luispedro.org>
2013-07-20 13:39:40 +0200

    TST Fix testing paths

---
+++ merge/__init__.py
@@ -0,0 +1,1 @@
+from .merge import hash_file, props_for, merge, parse_options
---
+++ merge/tests/test_merge.py
@@ -0,0 +1,24 @@
+import os
+import merge
+def test_hash_file():
+    h = merge.hash_file('merge/tests/data/A/3')
+    h2 = merge.hash_file('merge/tests/data/A/3')
+    assert h == h2
+    assert type(h) is str
+
+def test_props_for():
+    p = merge.props_for('merge/tests/data/A/3')
+    p2 = merge.props_for('merge/tests/data/A/3')
+    assert p == p2
+    p2 = merge.props_for('merge/tests/data/B/3')
+    assert p != p2
+
+def test_rsync_copy():
+    p = merge.props_for('merge/tests/data/A/1')
+    p2 = merge.props_for('merge/tests/data/B/1')
+    assert p == p2
+
+def test_merge():
+    options,_ = merge.parse_options(['merge'])
+    for op, args in merge.merge('merge/tests/data/A', 'merge/tests/data/B', options):
+        assert op in (os.rename, os.unlink)
