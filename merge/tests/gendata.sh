36bd1039515dfa5240345a5f9bc4e9b35ab570ca
Luis Pedro Coelho <luis@luispedro.org>
2020-07-26 01:31:02 +0200

    TST Noisier tests

--- merge/tests/gendata.sh
+++ merge/tests/gendata.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 mkdir data
 cd data
+
 mkdir A
 cd A
 for i in `seq 20`; do
     echo $i > $i
 done
 cd ..
+
 rsync --archive A/ B
 sleep 2
 touch B/3
+
+rsync --archive A/ C
+mkdir C/.git
+echo "Hello World" > C/.git/hello
+
 cd ..
 
+echo "Created test directories"
+find
