6e441405cd9860225dc77dc9b0102badd1092184
Luis Pedro Coelho <luis@luispedro.org>
2020-07-27 03:23:46 +0200

    TST Use new directory name

--- mergedirs/tests/test_merge.py
+++ mergedirs/tests/test_merge.py
@@ -1,20 +1,20 @@
 import os
-import merge
+import mergedirs
 def test_hash_file():
-    h = merge.hash_file('mergedirs/tests/data/A/3')
-    h2 = merge.hash_file('mergedirs/tests/data/A/3')
+    h = mergedirs.hash_file('mergedirs/tests/data/A/3')
+    h2 = mergedirs.hash_file('mergedirs/tests/data/A/3')
     assert h == h2
 
 def test_props_for():
-    p = merge.props_for('mergedirs/tests/data/A/3')
-    p2 = merge.props_for('mergedirs/tests/data/A/3')
+    p = mergedirs.props_for('mergedirs/tests/data/A/3')
+    p2 = mergedirs.props_for('mergedirs/tests/data/A/3')
     assert p == p2
-    p2 = merge.props_for('mergedirs/tests/data/B/3')
+    p2 = mergedirs.props_for('mergedirs/tests/data/B/3')
     assert p != p2
 
 def test_rsync_copy():
-    p = merge.props_for('mergedirs/tests/data/A/1')
-    p2 = merge.props_for('mergedirs/tests/data/B/1')
+    p = mergedirs.props_for('mergedirs/tests/data/A/1')
+    p2 = mergedirs.props_for('mergedirs/tests/data/B/1')
 
     assert p[:3] == p2[:3]
     # Rsync does not always copy the full timestamp, but it should copy the
@@ -22,6 +22,6 @@ def test_rsync_copy():
     assert int(p[3]) == int(p2[3])
 
 def test_merge():
-    options,_ = merge.parse_options(['merge'])
-    for op in merge.merge('mergedirs/tests/data/A', 'mergedirs/tests/data/B', options):
+    options,_ = mergedirs.parse_options(['merge'])
+    for op in mergedirs.merge('mergedirs/tests/data/A', 'mergedirs/tests/data/B', options):
         assert op.f in (os.rename, os.unlink)
